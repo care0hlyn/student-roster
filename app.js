b657f5b17bf8a410f1f18f62e08d45115d27a818
Carolyn <ckhuynh20@gmail.com>
2014-12-12 16:41:24 -0800

    Add route config, add new student view

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var studentRoster = angular.module('studentRoster', []);
+
+studentRoster.config(function($routeProvider){
+  $routeProvider
+    .when('/'
+    {
+      controller: 'StudentsCtrl',
+      templateUrl: 'views/Students.html'
+    })
+
+});
